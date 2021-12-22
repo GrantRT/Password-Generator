b89deb2e788b5ce9cd5b16f0f90b87f2cb1e1449
GrantRT <granto_92@hotmail.com>
2021-12-22 19:48:15 +0000

    started creating function

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
+// Creating strings for lowercase and uppercase letters, numbers and special characters
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbers = '0123456789';
+var special = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+
+function generatePassword() {
+  var length = prompt('How long would you like your password to be? Between 8-128 characters)');
+  var num = Number(length);
+  if (length < 8) {
+    alert('Your password cannot be less than 8 charcters long');
+  } else if (length > 128) {
+    alert('Your password cannot be more than 128 charcters long');
+  } else Number(length);
+  console.log(num);
+
+  return 'Generated password here';
+}
+
+// Prompt to determine length of password
+
+// Prompts with other options
+// var lower = confirm('Would you like your password to contain lowercase letters?');
+// var upper = confirm('Would you like your password to contain uppercase letters?');
+// var num = confirm('Would you like your password to contain numbers?');
+// var spec = confirm('Would you like your password to contain special characters?');
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
