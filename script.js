073a1fa6cdfa0c1644f7ee120f2026be4208045f
GrantRT <granto_92@hotmail.com>
2022-01-05 12:49:10 +0000

    changed strings to arrays and added confrim prompts

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
+// Empty array for the users choices from the prompts
+var userChoices = [];
+
+// Creating arrays for lowercase and uppercase letters, numbers and special characters
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var special = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
-
-// Creating strings for lowercase and uppercase letters, numbers and special characters
-var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var numbers = '0123456789';
-var special = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-
 function generatePassword() {
   var length = prompt('How long would you like your password to be? Between 8-128 characters)');
   var num = Number(length);
-  if (length < 8) {
+  if (num < 8) {
     alert('Your password cannot be less than 8 charcters long');
-  } else if (length > 128) {
+  } else if (num > 128) {
     alert('Your password cannot be more than 128 charcters long');
-  } else Number(length);
+  } else if (isNaN(num)) {
+    alert('Please enter a numerical value');
+  } else Number(num);
   console.log(num);
+
+  if (confirm('Would you like your password to contain lowercase letters?')) {
+    userChoices = userChoices.concat(lowercase);
+  }
+
+  if (confirm('Would you like your password to contain uppercase letters?')) {
+    userChoices = userChoices.concat(uppercase);
+  }
+
+  if (confirm('Would you like your password to contain numbers?')) {
+    userChoices = userChoices.concat(numbers);
+  }
+
+  if (confirm('Would you like your password to contain special characters?')) {
+    userChoices = userChoices.concat(special);
+  }
 
   return 'Generated password here';
 }
-
-// Prompt to determine length of password
-
-// Prompts with other options
-// var lower = confirm('Would you like your password to contain lowercase letters?');
-// var upper = confirm('Would you like your password to contain uppercase letters?');
-// var num = confirm('Would you like your password to contain numbers?');
-// var spec = confirm('Would you like your password to contain special characters?');
 
 // Write password to the #password input
 function writePassword() {
