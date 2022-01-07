1a9201b79cad71605bd3510c6e159b6321e37f9f
GrantRT <granto_92@hotmail.com>
2022-01-07 10:36:08 +0000

    cleaned up code and added alert if no options were selected

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Empty array for the users choices from the prompts
-var userChoices = [];
-
 // Creating arrays for lowercase and uppercase letters, numbers and special characters
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -11,10 +8,11 @@ var special = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', 
 var generateBtn = document.querySelector('#generate');
 
 function generatePassword() {
-  // resets the users choice array
-  userChoices = [];
+  // Creates a new empty array
+  var userChoices = [];
 
-  var length = prompt('How long would you like your password to be? Between 8-128 characters)');
+  // Prompt to determine length of the password
+  var length = prompt('How long would you like your password to be? (Between 8-128 characters)');
   var num = Number(length);
   if (num < 8) {
     alert('Your password cannot be less than 8 charcters long');
@@ -25,9 +23,9 @@ function generatePassword() {
   } else if (isNaN(num)) {
     alert('Please enter a numerical value');
     return 'Please try again';
-  } else Number(num);
-  console.log(num);
+  }
 
+  // Confirm prompts to determine what characters are used in the password
   if (confirm('Would you like your password to contain lowercase letters?')) {
     userChoices = userChoices.concat(lowercase);
   }
@@ -44,8 +42,13 @@ function generatePassword() {
     userChoices = userChoices.concat(special);
   }
 
+  if (userChoices.length === 0) {
+    alert('You must select at least one type of character');
+    return 'Please try again';
+  }
+
   var userPassword = '';
-  // for loop to run the character length selected by the user
+  // For loop to run the character length selected by the user
   for (var i = 0; i < num; i++) {
     var randomChar = Math.floor(Math.random() * userChoices.length);
     userPassword += userChoices[randomChar];
